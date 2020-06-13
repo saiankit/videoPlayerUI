5a9679fd21b524f2c82a9a8ad73572a9fbeed02c
saiankit <saiankit30@gmail.com>
2020-06-13 22:31:43 +0530

    Added the folders and header bar

---
+++ lib/utilities/theme_style.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ThemeStyle {
+  static final TextStyle appBarLabelG = TextStyle(
+    color: Colors.red,
+    fontSize: 30,
+    fontFamily: 'futura'
+  );
+  static final TextStyle appBarLabelN = TextStyle(
+    color: Colors.blueAccent,
+    fontSize: 30,
+    fontFamily: 'futura'
+  );
+  static final TextStyle appBarLabelText = TextStyle(
+    color: Colors.black,
+    fontSize: 30,
+    fontFamily: 'futura'
+  );
+}
