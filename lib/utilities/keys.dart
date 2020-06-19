5776cf38eba75c9b7832573aba4b8cd3825ad4fd
saiankit <saiankit30@gmail.com>
2020-06-19 07:39:43 +0530

    Added keys.dart file

---
+++ lib/utilities/keys.dart
@@ -0,0 +1,10 @@
+//Youtube Auth Key of Sai Ankit
+final String authKey = 'AIzaSyBJ1-EbO13qgFP6XyxyuaZJK_904hvnT80';
+// final String authKey = '';
+//The Channel ID's of all GNN arms
+final String mainChannelID = 'UC2k2cBpOZyxRup8r2CA_0SA';
+// final String mainChannelID = '';
+final String politicsChannelID = 'UCQXPJ1KUuLQjCCdxNMqLmEg';
+final String filmDhabaChannelID = 'UCRLOCdnLWoftCPrqLtAJxLQ';
+final String foodAndHealthChannelID = 'UC4qD-FckBQBU_YEyWg9sOAw';
+final String devotionalChannelID = 'UCaD73LgoHV_v-XmYUi1yFSQ';
